3ac71152995e384f7721f5531e8f3f202111fb5a
Claire Pham <hqp002@ucsd.edu>
2017-03-08 01:46:21 -0800

    changed insert.php and process.php

--- php/insert.php
+++ php/insert.php
@@ -13,26 +13,24 @@ try {
     echo "Connected successfully"; 
     }
 
+//Creating Variables Q: is this necessary?
+// $imgURL=$_POST['imgURL']; commenting out bc unsure whether or not this is right
+$clothesType=$_POST['clothesType'];
+
 //Insert Data
-/* 3/8 Q: if i want imgURL to be added to the same clothesID as clothesType, how would i do that? per our conversation yesterday, you recommended against having one php file that would handle all of this. should i have scandir.php for example render this page right after?  */
+/* Q: if i want imgURL to be added to the same clothesID as clothesType, how would i do that? per our conversation yesterday, you recommended against having one php file that would handle all of this. should i have scandir.php for example render this page right after?  */
 $sql="INSERT INTO clothesWardrobe (clothesType)
 	VALUES('$_POST[fname]')
 ";
 
-if(!mysql_query($sql, $conn))
-{
-	die('Error: ' .mysql_error());
+//added condition if pass/fail
+if(mysqli_affected_rows($connect)>0){
+	echo "echo Success! <br>";
+	echo "Go back to <a href="uploadimage.html">upload image</a> Or <a href="home.html">main menu</a>?";
 }
-
-/*
-3/8 notes. for future reference, user friendly language should appear something like: Success! Go back to upload image? Or main menu? should i echo a success msg in insert.php or process.php?
-
-Other notes: Pages should be able to load without having to refresh. Work on ajax tonight. 
-
-echo "Success! <br>;
-echo "Go back to <a href='uploadimage.html'>upload image</a>? Or <a href='home.html'>main menu</a>?";
-*/
-
+else{
+	echo "Adding to category NOT added. <br>";
+	echo mysqli_error($connect);
 
 //Catch block handles any problems that occur in DB queries
 catch(PDOException $e)
